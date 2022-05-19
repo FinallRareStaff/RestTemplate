c46847e1f91c5df6438550453f55af8e2090f720
FinallRareStaff <Jeymester@gmail.com>
2022-05-19 20:20:35 +0300

    Decision

--- src/main/java/com/example/resttemplate/Application.java
+++ src/main/java/com/example/resttemplate/Application.java
@@ -1,24 +1,19 @@
 package com.example.resttemplate;
 
+import com.example.resttemplate.configuration.Config;
+import com.example.resttemplate.model.User;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
 
-import java.util.ArrayList;
-import java.util.List;
-
-@Configuration
-@ComponentScan("com.example.resttemplate")
 public class Application {
     public static void main(String[] args) {
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Application.class);
-        Requests requests = context.getBean(Requests.class);
-        System.out.println(requests.getAllUsers());
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
+        Communication communication = context.getBean(Communication.class);
+        System.out.println(communication.getAllUsers());
+        User user = new User(3L, "James", "Brown", (byte) 22);
+        User thomas = new User(3L, "Thomas", "Shelby", (byte) 22);
+        communication.saveUser(user);
+        communication.updateUser(thomas);
+        communication.deleteUser((byte) 3);
     }
 
 }
---
+++ src/main/java/com/example/resttemplate/Communication.java
@@ -0,0 +1,68 @@
+package com.example.resttemplate;
+
+
+import com.example.resttemplate.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.*;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+@Component
+public class Communication {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    private final String URL = "http://94.198.50.185:7081/api/users";
+    private final HttpHeaders httpHeaders = new HttpHeaders();
+
+
+    public List<User> getAllUsers(){
+        ResponseEntity<List<User>> response =
+                restTemplate.exchange(URL,
+                        HttpMethod.GET,
+                        null,
+                        new ParameterizedTypeReference<>() {});
+        HttpHeaders headers = response.getHeaders();
+        String cookie = headers.get("Set-cookie").get(0);
+        cookie = "JSESSIONID=" + cookie.substring(cookie.indexOf("=")+1, cookie.indexOf(";"));
+        httpHeaders.add("Cookie", cookie);
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+        return response.getBody();
+    }
+
+    public void saveUser(User user) {
+        HttpEntity<User> request = new HttpEntity<>(user, httpHeaders);
+        ResponseEntity<String> response =
+                restTemplate.exchange(URL,
+                        HttpMethod.POST,
+                        request,
+                        String.class);
+        response.getBody();
+    }
+
+    public void updateUser(User user) {
+        HttpEntity<User> request = new HttpEntity<>(user, httpHeaders);
+        ResponseEntity<String> response =
+                restTemplate.exchange(URL,
+                        HttpMethod.PUT,
+                        request,
+                        String.class);
+        response.getBody();
+    }
+
+    public void deleteUser(byte id) {
+        HttpEntity<User> request = new HttpEntity<>(httpHeaders);
+        ResponseEntity<String> response =
+                restTemplate.exchange(URL + "/" + id,
+                        HttpMethod.DELETE,
+                        request,
+                        String.class);
+        response.getBody();
+    }
+
+
+}
